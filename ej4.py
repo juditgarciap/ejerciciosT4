46ece5a3495563e27a9e0e6f27bf4d7a08405c00
juditgarciap <judit.garciapadronleon@usp.ceu.es>
2022-11-29 10:37:39 +0100

    ej4

--- ej4.py
+++ ej4.py
@@ -10,3 +10,18 @@ def biseccion(f, a, b, tol, iteracion=1):
         # se deben obtener signos diferentes, esto es f(a)*f(b) < 0.
         print("En el intervalo especificado no existe raíz para la función.")
      
+
+    # Paso de la bisección: Dividir el intervalo en un punto medio.
+    x = (a + b)/2
+    
+    # Finalizar si se cumple la tolerancia,
+    # y por lo tanto retornar la raíz estimada y el número de iteraciones.
+    if -tol < f(x) < tol:
+        return x, iteracion
+    
+    # Preguntar si x es una mejora del intervalo por la izquierda f(a),
+    # esto es, si f(x) no cambia de signo con respecto a f(a). 
+    elif f(a)*f(x) >= 0:
+     # Actualizar intervalo y realizar llamada recursiva de la función.
+        return biseccion(f, x, b, tol, iteracion+1)
+    
